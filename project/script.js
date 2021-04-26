f5d541cf30a20fe9843912da7484d53f755de57e
ElieZeinoun <ez2265@columbia.edu>
2021-04-26 16:14:07 -0400

    updated-calculator
    calculator updates

--- project/script.js
+++ project/script.js
@@ -2,131 +2,131 @@
 
 let materials= [
  { 
-    name:"concrete",
+    name:"carpet",
     id: 1,
-    application: ["all", "walls", "floors"],
-    carbon: 550,
+    application: ["all", "furn", "floors"],
+    carbon: 150,
     cost: 45,
     image: "assets/carpet-1.jpg",
  },
  {  
-    name:"wood",
+    name:"carpet",
     id: 2,
-    application: ["all", "walls"],
-    carbon: 750,
-    cost: 20,
+    application: ["all", "furn", "floors"],
+    carbon: 70,
+    cost: 30,
     image: "assets/carpet-2.jpg",
  },
  {  
-    name:"ceramic",
+    name:"wood",
     id: 3,
-    application: ["all", "walls", "floors", "furniture"],
-    carbon: 750,
-    cost: 20,
+    application: ["all", "walls", "floors", "furn"],
+    carbon: 600,
+    cost: 60,
     image: "assets/wood-1.jpg",
  },
  {  
     name:"wood",
     id: 4,
     application: ["all", "walls", "floors"],
-    carbon: 750,
-    cost: 20,
+    carbon: 250,
+    cost: 25,
     image: "assets/wood-2.jpg",
  },
  {  
    name:"wood",
    id: 5,
-   application: ["all", "walls", "floors"],
-   carbon: 750,
-   cost: 20,
+   application: ["all", "walls", "floors", "furn"],
+   carbon: 350,
+   cost: 75,
    image: "assets/wood-3.jpg",
 },
 {  
-   name:"wood",
+   name:"ceramic",
    id: 6,
    application: ["all", "walls", "floors"],
-   carbon: 750,
-   cost: 20,
+   carbon: 400,
+   cost: 15,
    image: "assets/ceramic-1.jpg",
 },
 {  
-   name:"wood",
+   name:"leather",
    id: 7,
-   application: ["all", "walls", "floors"],
-   carbon: 750,
-   cost: 20,
+   application: ["all", "furn"],
+   carbon: 1500,
+   cost: 150,
    image: "assets/leather-1.jpg",
 },
 {  
-   name:"wood",
+   name:"ceramic",
    id: 8,
    application: ["all", "walls", "floors"],
-   carbon: 750,
-   cost: 20,
+   carbon: 550,
+   cost: 200,
    image: "assets/ceramic-2.jpg",
 },
 {  
-   name:"wood",
+   name:"cotton",
    id: 9,
-   application: ["all", "walls", "floors"],
+   application: ["all", "furn"],
    carbon: 750,
-   cost: 20,
+   cost: 80,
    image: "assets/cotton-1.jpg",
 },
 {  
-   name:"wood",
+   name:"mdf",
    id: 10,
-   application: ["all", "walls", "floors"],
-   carbon: 750,
-   cost: 20,
-   image: "assets/mdf-1.jpg",
+   application: ["all", "walls"],
+   carbon: 250,
+   cost: 65,
+   image: "assets/paint-2.jpg",
 },
 {  
-   name:"wood",
+   name:"fabric",
    id: 11,
-   application: ["all", "walls", "floors"],
+   application: ["all", "furn"],
    carbon: 750,
-   cost: 20,
+   cost: 55,
    image: "assets/fabric-1.jpg",
 },
 {  
-   name:"wood",
+   name:"marble",
    id: 12,
-   application: ["all", "walls", "floors"],
-   carbon: 750,
-   cost: 20,
+   application: ["all", "furn", "floors"],
+   carbon: 200,
+   cost: 40,
    image: "assets/marble-1.jpg",
 },
 {  
-   name:"wood",
+   name:"glass",
    id: 13,
-   application: ["all", "walls", "floors"],
-   carbon: 750,
-   cost: 20,
+   application: ["all", "walls", "furn"],
+   carbon: 300,
+   cost: 100,
    image: "assets/glass.jpg",
 },
 {  
-   name:"wood",
+   name:"pvc",
    id: 14,
-   application: ["all", "walls", "misc"],
+   application: ["all", "furn", "floor"],
    carbon: 750,
-   cost: 20,
+   cost: 220,
    image: "assets/pvc-1.jpg",
 },
 {  
-   name:"wood",
+   name:"cotton",
    id: 15,
-   application: ["all", "misc",],
-   carbon: 750,
-   cost: 20,
+   application: ["all", "furn"],
+   carbon: 500,
+   cost: 25,
    image: "assets/towel-1.jpg",
 },
 {  
-   name:"wood",
+   name:"textile",
    id: 16,
-   application: ["all", "walls", "floors"],
-   carbon: 750,
-   cost: 29,
+   application: ["all", "furn"],
+   carbon: 550,
+   cost: 95,
    image: "assets/textile-1.jpg",
 },
 ]
@@ -137,60 +137,30 @@ let container = document.querySelector(".main");
 
 function renderAllImages(data){
     data.forEach(function(material) {
+        let mat_id = material.id;
         let divjs = document.createElement("div");
+
         let renderImage = document.createElement("img");
         let srcImage = material.image;
         let matname = material.name;
-        renderImage.className = "imgjs", `${matname}`;        ;
+        let matcost = material.cost;
+      //   console.log(matcost);
+
+        renderImage.classList.add("imgjs", matname);
         renderImage.src = srcImage;
+        renderImage.id = mat_id;
         divjs.appendChild(renderImage);
         container.appendChild(divjs);
+      //   console.log(renderImage);
     });
 } 
 
-renderAllImages(materials);
 
-let costcontainer = document.querySelector("#price");
 
-function calculator(numbers){
-
-      numbers.forEach(function(element){
-         let value = element.cost;
-         console.log(value);
-         let cost = document.createElement("p");
-         let printcost = document.createTextNode(value);
-         let costinp = cost.appendChild(printcost);
-         costcontainer.appendChild(cost);
-      });
-   
-}
 
 $(document).ready(function(){
 
-   $(".imgjs").draggable(
-      {
-         helper:'clone',
-         containment: "wrapper", scroll: false,
-         revert: "invalid",
-         cursor: "move",  
-      }
-   );
-
-   $(".cart").droppable(
-      {
-         accept:".imgjs",
-         drop: function (ev,ui) {
-            var droppedItem = $(ui.draggable).clone();
-            console.log(ev);
-            console.log(ui);
-            $(this).append(droppedItem);
-            $(droppedItem).addClass("incart");
-            $(this).add('dropped_array');
- 
-         }
-
-      }
-   )
+   renderAllImages(materials);
    
    $('.list').click(function(){
 
@@ -210,37 +180,155 @@ $(document).ready(function(){
 
          if (mat_list.includes(value)){
             filtered.push(materials[i]);
-         }        
+          }        
 
       }
 
       renderAllImages(filtered);
       
 
-      // if (value == 'all'){
-      //    $(materials).show('1000');
-      // }
-      // else{
-      //    $(application).not(value).hide('1000');
-      //    $(application).filter(value).show('1000');
-      // }
-   })
+      $(".imgjs").draggable(
+         {
+            helper:'clone',
+            containment: "wrapper", scroll: false,
+            revert: "invalid",
+            cursor: "move",  
+            zIndex: 100,
+         }
+      );
+   
+      $(".cart").droppable(
+         {
+            accept:".imgjs",
+            drop: function (ev,ui) {
+               let drop_id = ui.draggable.attr('id');
+               // let drop_cost = ui.draggable.attr('cost');
+               // console.log(materials.cost);
 
+               var droppedItem = $(ui.draggable).clone();
+               var div_child_count = $(ev.target).children().length;
+               var div_count = $(ev.target).length;
+
+               //Check if existing element has already been dropped
+               if(div_child_count <div_count){
+                  $(this).append(droppedItem);
+                  $(droppedItem).addClass("incart");
+                  $(droppedItem).addClass("price");
+                  // console.log(ev.target.children)
+                  
+                  cart_items =[];
+                  let totalvalues =[];
+
+                        function calculator(numbers){
+
+                           // let totalvalues = [25, 40];
+                     
+                           numbers.forEach(function(element){
+                              if(element.id == drop_id){
+                                 cart_items.push(element);
+
+                                 let value = element.cost;
+                                 // console.log(value);
+                                 // let cost = document.createElement("p");
+                                 // let printcost = document.createTextNode(value);
+                                 // let costinp = cost.appendChild(printcost);
+                                 // costcontainer.appendChild(cost);
+                                 totalvalues.push(value);
+                                 console.log(totalvalues);
+                                 
+
+                              }
+                              
+                           });
+
+                           let costtotal = document.querySelectorAll('.price')
+                           costtotal.forEach(function(e){
+                              console.log(e);
+                           })
+                           // console.log(costtotal);
+                           
+                           const sum = totalvalues.reduce((accumulator, currentValue) => accumulator + currentValue);
+                           console.log(sum);
+                     
+                           let cost = document.querySelector("#price");
+                           // cost.classList.add('price');
+                           let printcost = document.createTextNode(sum);
+                           let costinp = cost.appendChild(printcost);
+                           // costcontainer.appendChild(cost);
+                     }
+                     
+                     calculator(materials);
+                     
+
+                     }             
+
+                 
+                  
+                  
+                  }
+            })
+         }
+      )
+   
+
+
+//Empty Cart from Item when double clicked
+   $(".cart").dblclick(function(){
+      console.log(this);
+      $(this).empty();      
+   });
+//..............................................
+
+
+//Add Active Class to selected List Item & Remove Class from Siblings
    $('.list').click(function(){
       $(this).addClass('active').siblings().removeClass('active');
+   })
+//..............................................
+
+
+   $('.imgjs').click(function(){
+      console.log(this);
+      var img = materials[$(this).index()];
+      
+      console.log(img);
    })
 })
 
 
+//Calculator Code
+let costcontainer = document.querySelector("#price");
 
+// function calculator(numbers){
 
+//       let totalvalues = [];
+
+//       numbers.forEach(function(element){
+//          let value = element.cost;
+//          // console.log(value);
+//          // let cost = document.createElement("p");
+//          // let printcost = document.createTextNode(value);
+//          // let costinp = cost.appendChild(printcost);
+//          // costcontainer.appendChild(cost);
+//          totalvalues.push(value);
+//       });
+
+      
+//       const sum = totalvalues.reduce((accumulator, currentValue) => accumulator + currentValue);
+//       console.log(sum);
+
+//       let cost = document.createElement("p");
+//       let printcost = document.createTextNode(sum);
+//       let costinp = cost.appendChild(printcost);
+//       costcontainer.appendChild(cost);
+// }
 
 // calculator(materials);
 
 
-// const sum = costprice.reduce((accumulator, currentValue) => accumulator + currentValue);
 
-// console.log(sum)
+
+
 
 
 
